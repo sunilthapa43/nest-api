f7d4cfec78ccbe725daf631ca2a4ff8970f8bbde
Sunil Thapa <tsunil359@gmail.com>
2022-06-18 14:10:56 +0545

    automate db restart and prisma migrations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,34 +1,51 @@
-import { Injectable, Post } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { AuthDto } from "./dto";
-import * as argon from "argon2";
-
-
+import { ForbiddenException, Injectable, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
-export class AuthService{
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService){
-
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials Incorrect');
     }
 
-   signin(dto:AuthDto){
-        return {
-            msg: "I am logged in"
-        };
-    }
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
 
-    
-    async signup(dto : AuthDto){
+    delete user.hash;
+    return user;
+  }
 
-        const hash = await argon.hash(dto.password);
-        const user  = await this.prisma.user.create({
-            data:{
-                email: dto.email,
-                hash,
-            }
-        })
-        return user;
-        
+  
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        } else {
+          throw error;
+        }
+      }
     }
+  }
 }

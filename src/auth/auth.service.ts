ec050094d6d727620e8263f31fe38ee2e1c8cb75
Sunil Thapa <tsunil359@gmail.com>
2022-06-18 14:56:43 +0545

    added jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signin(dto: AuthDto) {
     const user = await this.prisma.user.findUnique({
@@ -21,11 +27,9 @@ export class AuthService {
     const pwMatches = await argon.verify(user.hash, dto.password);
     if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
 
-    delete user.hash;
-    return user;
+    
+    return this.signToken(user.id, user.email);
   }
-
-  
 
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
@@ -36,8 +40,8 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
-      return user;
+    
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code == 'P2002') {
@@ -47,5 +51,22 @@ export class AuthService {
         }
       }
     }
+  }
+
+  async signToken(userId: number, email: string): Promise<{access_token: string}> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token =  await this.jwt.signAsync(payload, {
+        expiresIn: '15m',
+        secret: secret,
+      });
+
+    return {
+        access_token: token,
+    };
+       
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient{
 
-    constructor(){
+    constructor(private config: ConfigService){
         super({
             datasources: {
                 db:{
-                    url: 'postgresql://postgres:1234@localhost:5434/nest?schema=public'
+                    url: config.get('DATABASE_URL'),
                 }
             }
-        })
+        });
+        
     }
+    
 }

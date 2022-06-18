86b84bdd28eaa7575ad8fd84f3d16de4c2f34dcf
Sunil Thapa <tsunil359@gmail.com>
2022-06-18 09:09:13 +0545

    config docker, Prisma orm

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+
+
+@Injectable({})
+export class AuthService{
+
+    constructor(private prisma: PrismaService){
+        
+    }
+
+   signin(){
+        return {
+            msg: "I am logged in"
+        };
+    }
+
+
+    signup(){
+        return{
+            msg: "I am signed up"
+            
+        }
+    }
+}

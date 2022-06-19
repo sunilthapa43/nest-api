ab4de3114363eb5b502f478fdd3e8ebc09bef6fd
Sunil Thapa <tsunil359@gmail.com>
2022-06-19 15:42:49 +0545

    final commit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestApplication } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
+//import { AppModule } from '../src/app.module';
 
 describe('App e2e', () => {
   let app: NestApplication;
